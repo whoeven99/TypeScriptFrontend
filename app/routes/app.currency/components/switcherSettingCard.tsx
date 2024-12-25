4fe01b484b9008371e299bbe1c21ddbc4d9bd860
wanggongwei <wgw1476269816@gmail.com>
2024-12-25 15:59:50 +0800

    #

--- app/routes/app.currency/components/switcherSettingCard.tsx
+++ app/routes/app.currency/components/switcherSettingCard.tsx
@@ -60,90 +60,86 @@ const SwitcherSettingCard: React.FC<SwitcherSettingCardProps> = ({
   }, [moneyWithCurrencyFormatHtml, moneyFormatHtml]);
 
   return (
-    <div>
-      {isVisible && (
-        <Card >
-          <Space direction="vertical" size="small" style={{ display: "flex" }}>
-            <div className="card-header">
-              <Title style={{ fontSize: "1.25rem", display: "inline" }}>
-                Step 1: Set up Currency Format
-              </Title>
-
-              <Link url={settingUrl} target="_blank">
-                <Button type="primary" className="currency-action">
-                  Setup
-                </Button>
-              </Link>
-            </div>
-            <Text>
-              To display currency switcher, please follow the instructions
-              below:
-            </Text>
+    <Space direction="vertical" size="small" style={{ display: "flex" }}>
+      <Card>
+        <Space direction="vertical" size="small" style={{ display: "flex" }}>
+          <div className="card-header">
+            <Title style={{ fontSize: "1.25rem", display: "inline" }}>
+              Step 1: Set up Currency Format
+            </Title>
+            {isVisible ? <Text> Completed</Text> : <Text> Uncompleted</Text>}
+            <Link url={settingUrl} target="_blank">
+              <Button type="primary" className="currency-action">
+                Setup
+              </Button>
+            </Link>
+          </div>
+          <Text>
+            To display currency switcher, please follow the instructions below:
+          </Text>
+          <div>
+            <Text>1. Go to </Text>
+            <Link url={settingUrl} target="_blank">
+              Settings {">>"} General
+            </Link>
+          </div>
+          <Text>
+            2. Under the <strong>’Store defaults‘</strong> section, click Change
+            currency formatting, then change with the code below:
+          </Text>
+          {isWithMoneyVisible && (
             <div>
-              <Text>1. Go to </Text>
-              <Link url={settingUrl} target="_blank">
-                Settings {">>"} General
-              </Link>
+              <strong>HTML with currency:</strong>
+              <Paragraph
+                copyable={{
+                  text: `<span class="ciwi-money">${moneyWithCurrencyFormatHtml}</span>`,
+                }}
+              >
+                &lt;span class="ciwi-money"&gt;{moneyWithCurrencyFormatHtml}
+                &lt;/span&gt;
+              </Paragraph>
             </div>
-            <Text>
-              2. Under the <strong>’Store defaults‘</strong> section, click
-              Change currency formatting, then change with the code below:
-            </Text>
-            {isWithMoneyVisible && (
-              <div>
-                <strong>HTML with currency:</strong>
-                <Paragraph
-                  copyable={{
-                    text: `<span class="ciwi-money">${moneyWithCurrencyFormatHtml}</span>`,
-                  }}
-                >
-                  &lt;span class="ciwi-money"&gt;{moneyWithCurrencyFormatHtml}
-                  &lt;/span&gt;
-                </Paragraph>
-              </div>
-            )}
-            {isWithoutMoneyVisible && (
-              <div>
-                <strong>HTML without currency:</strong>
-                <Paragraph
-                  copyable={{
-                    text: `<span class="ciwi-money">${moneyFormatHtml}</span>`,
-                  }}
-                >
-                  &lt;span class="ciwi-money"&gt;{moneyFormatHtml}
-                  &lt;/span&gt;
-                </Paragraph>
-              </div>
-            )}
-          </Space>
-        </Card>
-      )}
-      {isEnable && (
-        <Card>
-          <Space direction="vertical" size="small" style={{ display: "flex" }}>
-            <div className="card-header">
-              <Title style={{ fontSize: "1.25rem", display: "inline" }}>
-                Step 2: Enable switcher
-              </Title>
+          )}
+          {isWithoutMoneyVisible && (
+            <div>
+              <strong>HTML without currency:</strong>
+              <Paragraph
+                copyable={{
+                  text: `<span class="ciwi-money">${moneyFormatHtml}</span>`,
+                }}
+              >
+                &lt;span class="ciwi-money"&gt;{moneyFormatHtml}
+                &lt;/span&gt;
+              </Paragraph>
             </div>
-            <Text>
-              Please{" "}
-              <Link url={blockUrl} target="_blank">
-                Click here
-              </Link>{" "}
-              to go to Shopify theme editor {">>"} enable "Ciwi_Switcher" {">>"}{" "}
-              click the "Save" button in the right corner.
-            </Text>
-            <Text>
-              Please refer to this {" "}
-              <Link url={supportUrl} target="_blank">
-                step-by-step guide
-              </Link>
-            </Text>
-          </Space>
-        </Card>
-      )}
-    </div>
+          )}
+        </Space>
+      </Card>
+      <Card>
+        <Space direction="vertical" size="small" style={{ display: "flex" }}>
+          <div className="card-header">
+            <Title style={{ fontSize: "1.25rem", display: "inline" }}>
+              Step 2: Enable switcher
+            </Title>
+            <Text>{isEnable ? "Completed" : "Uncompleted"}</Text>
+          </div>
+          <Text>
+            Please{" "}
+            <Link url={blockUrl} target="_blank">
+              Click here
+            </Link>{" "}
+            to go to Shopify theme editor {">>"} enable "Ciwi_Switcher" {">>"}{" "}
+            click the "Save" button in the right corner.
+          </Text>
+          <Text>
+            Please refer to this {" "}
+            <Link url={supportUrl} target="_blank">
+              step-by-step guide
+            </Link>
+          </Text>
+        </Space>
+      </Card>
+    </Space>
   );
 };
 
