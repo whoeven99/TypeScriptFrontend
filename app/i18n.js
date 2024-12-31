3acd95362e49088c5442862f857b934576974fbb
wanggongwei <wgw1476269816@gmail.com>
2024-12-31 10:30:31 +0800

    #

---
+++ app/i18n.js
@@ -0,0 +1,23 @@
+// src/i18n.js
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+// 初始化i18next
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "zh",
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+  });
+
+export default i18n;
