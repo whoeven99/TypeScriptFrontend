5c041488259f549231c2adc41c5ccf6f61a85cad
wanggongwei <wgw1476269816@gmail.com>
2025-01-07 12:10:16 +0800

    #

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -320,8 +320,6 @@ export default function App() {
           <Link to="/app/manage_translation">{t("Manage Translation")}</Link>
           <Link to="/app/currency">{t("Currency")}</Link>
           <Link to="/app/glossary">{t("Glossary")}</Link>
-          <Link to="/app/test1">{t("test1")}</Link>
-          <Link to="/app/test2">{t("test2")}</Link>
         </NavMenu>
         <Outlet />
       </ConfigProvider>
