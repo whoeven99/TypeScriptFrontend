b018c33780a82da9a9713449a6debf8e88bdfd35
wanggongwei <wgw1476269816@gmail.com>
2025-01-14 17:55:13 +0800

    #

--- app/root.tsx
+++ app/root.tsx
@@ -68,7 +68,7 @@ export default function App() {
     <Provider store={store}>
       <html lang={i18nCode}>
         <head>
-          ${styleText}
+          {styleText}
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
           {/* <meta name="shopify-debug" content="web-vitals" />
