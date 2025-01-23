85a69ff6706fb57a40580f4a11ad73acb517206a
wanggongwei <wgw1476269816@gmail.com>
2025-01-23 13:11:13 +0800

    限制更新

--- app/routes/app.language/route.tsx
+++ app/routes/app.language/route.tsx
@@ -538,7 +538,7 @@ const Index = () => {
 
   const handleTranslate = async (locale: string) => {
     if (words && words.chars > words.totalChars) {
-      message.error(t("Character Limit Reached"));
+      setShowWarnModal(true);
     } else {
       const selectedItem = data.find(
         (item: LanguagesDataType) => item.locale === locale,
@@ -635,10 +635,7 @@ const Index = () => {
   return (
     <>
       {showWarnModal && (
-        <TranslationWarnModal
-          show={showWarnModal}
-          setShow={setShowWarnModal}
-        />
+        <TranslationWarnModal show={showWarnModal} setShow={setShowWarnModal} />
       )}
       <Page>
         <TitleBar title={t("Language")} />
