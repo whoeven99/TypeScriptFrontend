2cdd0b70adc943475f6a8156d7093c0d3d228a8f
wanggongwei <wgw1476269816@gmail.com>
2025-02-19 13:24:51 +0800

    #

--- app/root.tsx
+++ app/root.tsx
@@ -249,7 +249,7 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <html lang={lang}>
+      <html lang={"zh-CN"}>
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
