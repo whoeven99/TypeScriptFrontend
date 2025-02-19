35496bc1e35c5732e9e175c401d33f439def233b
wanggongwei <wgw1476269816@gmail.com>
2025-02-19 10:35:25 +0800

    #

--- app/components/userGuideCard.tsx
+++ app/components/userGuideCard.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Card, Typography, List } from 'antd';
+import React, { Suspense, useEffect, useState } from 'react';
+import { Card, Typography, List, Skeleton } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { ReadOutlined, RightOutlined } from '@ant-design/icons';
 
@@ -40,19 +40,20 @@ const UserGuideCard: React.FC = () => {
     };
 
     return (
-        <Card
-            style={{
-                height: '100%',
-                width: '100%',
-                borderRadius: '8px',
-                boxShadow: '0 2px 8px rgba(0, 0, 0, 0.06)'
-            }}
-        >
-            <Text strong style={{ marginBottom: '16px' }}>
-                <ReadOutlined style={{ marginRight: '8px', color: '#007F61' }} />
-                {t('userGuide.title')}
-            </Text>
-            {/* 
+        <Suspense fallback={<Skeleton.Button active style={{ height: 150 }} block />}>
+            <Card
+                style={{
+                    height: '100%',
+                    width: '100%',
+                    borderRadius: '8px',
+                    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.06)'
+                }}
+            >
+                <Text strong style={{ marginBottom: '16px' }}>
+                    <ReadOutlined style={{ marginRight: '8px', color: '#007F61' }} />
+                    {t('userGuide.title')}
+                </Text>
+                {/* 
             <Text
                 style={{
                     display: 'block',
@@ -64,39 +65,40 @@ const UserGuideCard: React.FC = () => {
                 {t('userGuide.description')}
             </Text> */}
 
-            <List
-                itemLayout="horizontal"
-                dataSource={guideList}
-                renderItem={(item) => (
-                    <List.Item
-                        style={{
-                            padding: '4px 0',
-                            borderBottom: '1px solid #f0f0f0',
-                            cursor: 'pointer',
-                            transition: 'all 0.3s'
-                        }}
-                        onClick={() => handleOpenLink(item.link)}
-                    >
-                        <div style={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                            width: '100%',
-                            color: '#1890ff'
-                        }}>
-                            <Text style={{
-                                color: '#1890ff',
-                                width: 'calc(100% - 24px)', // 减去右箭头的宽度
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis',
-                                whiteSpace: 'nowrap'
-                            }}>{item.title}</Text>
-                            <RightOutlined style={{ fontSize: '12px' }} />
-                        </div>
-                    </List.Item>
-                )}
-            />
-        </Card>
+                <List
+                    itemLayout="horizontal"
+                    dataSource={guideList}
+                    renderItem={(item) => (
+                        <List.Item
+                            style={{
+                                padding: '4px 0',
+                                borderBottom: '1px solid #f0f0f0',
+                                cursor: 'pointer',
+                                transition: 'all 0.3s'
+                            }}
+                            onClick={() => handleOpenLink(item.link)}
+                        >
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                                width: '100%',
+                                color: '#1890ff'
+                            }}>
+                                <Text style={{
+                                    color: '#1890ff',
+                                    width: 'calc(100% - 24px)', // 减去右箭头的宽度
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                    whiteSpace: 'nowrap'
+                                }}>{item.title}</Text>
+                                <RightOutlined style={{ fontSize: '12px' }} />
+                            </div>
+                        </List.Item>
+                    )}
+                />
+            </Card>
+        </Suspense>
     );
 };
 
