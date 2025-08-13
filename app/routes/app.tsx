5eff0e1dae785555413c70e6ebe857e12fa6f4e8
wanggongwei <wgw1476269816@gmail.com>
2025-08-13 13:29:54 +0800

    #

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -246,28 +246,48 @@ export const action = async ({ request }: ActionFunctionArgs) => {
         });
 
         if (
-          shopLocalesIndex.includes(data.response?.target) &&
-          (data.response?.status !== 1 ||
+          shopLocalesIndex.includes(data.response[0]?.target) &&
+          (data.response[0]?.status !== 1 ||
             !shopLanguagesWithoutPrimaryIndex.find(
-              (item) => item.locale === data.response?.target,
+              (item) => item.locale === data.response[0]?.target,
             )?.published)
         ) {
           return {
-            translatingLanguage: {
-              source: data.response?.source || shopPrimaryLanguage[0].locale,
-              target: data.response?.target || "",
-              status: data.response?.status || 0,
-              resourceType: data.response?.resourceType || "",
-            },
+            translatingLanguage:
+              data.response.length > 0
+                ? data.response.map((item: any) => ({
+                    source: item?.source || shopPrimaryLanguage[0].locale,
+                    target: item?.target || "",
+                    status: item?.status || 0,
+                    resourceType: item?.resourceType || "",
+                  }))
+                : [
+                    {
+                      source: shopPrimaryLanguage[0].locale,
+                      target: "",
+                      status: 0,
+                      resourceType: "",
+                    },
+                  ],
           };
         } else {
           return {
-            translatingLanguage: {
-              source: data.response?.source || shopPrimaryLanguage[0].locale,
-              target: "",
-              status: 0,
-              resourceType: "",
-            },
+            translatingLanguage:
+              data.response.length > 0
+                ? data.response.map((item: any) => ({
+                    source: item?.source || shopPrimaryLanguage[0].locale,
+                    target: item?.target || "",
+                    status: item?.status || 0,
+                    resourceType: item?.resourceType || "",
+                  }))
+                : [
+                    {
+                      source: shopPrimaryLanguage[0].locale,
+                      target: "",
+                      status: 0,
+                      resourceType: "",
+                    },
+                  ],
           };
         }
       } catch (error) {
