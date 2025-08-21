b1f940cbb541cee11ed71641924ec3719ea064b8
wanggongwei <wgw1476269816@gmail.com>
2025-08-21 16:07:40 +0800

    #

--- app/routes/app.pricing/route.tsx
+++ app/routes/app.pricing/route.tsx
@@ -82,7 +82,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
                 : payForPlan.monthlyPrice,
               currencyCode: "USD",
             },
-            trialDays: payForPlan.trialDays,
+            trialDays: 1,
             returnUrl,
             test:
               process.env.NODE_ENV === "development" ||
