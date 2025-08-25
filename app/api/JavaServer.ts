54078e5a000649b15ae607dc5ec5006805dc864e
wanggongwei <wgw1476269816@gmail.com>
2025-08-25 19:14:49 +0800

    #

--- app/api/JavaServer.ts
+++ app/api/JavaServer.ts
@@ -397,7 +397,7 @@ export const SendSubscribeSuccessEmail = async ({
 
   try {
     const response = await axios({
-      url: `${process.env.SERVER_URL}/orders/sendSubscribeSuccessEmail`,
+      url: `${process.env.SERVER_URL}/orders/sendSubscribeSuccessEmail?shopName=${shopName}`,
       method: "POST",
       data: {
         id: id,
