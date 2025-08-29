017fb3c8047f1ea93b1c89b3984f2e5c37bfce3d
wanggongwei <1476269816@qq.com>
2025-08-29 23:52:17 +0800

    #

--- app/routes/app.switcher/route.tsx
+++ app/routes/app.switcher/route.tsx
@@ -268,7 +268,7 @@ const Index = () => {
         positionData: "10",
         isTransparent: false,
       };
-      if (data?.success && data.response !== undefined) {
+      if (data?.success) {
         const filteredResponse = Object.fromEntries(
           Object.entries(data.response).filter(([_, value]) => value !== null),
         );
