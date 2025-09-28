5889480e4ab4e8ea5974e671a16a03fba5c42794
wanggongwei <wgw1476269816@gmail.com>
2025-09-28 11:53:32 +0800

    更新LCP数据的打印

--- app/routes/web-vitals-metrics.tsx
+++ app/routes/web-vitals-metrics.tsx
@@ -7,7 +7,10 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
   const metrics = JSON.parse(formData.get("metrics") as string);
   //   const metrics = await request.json();
-  console.log(`${shop} received metrics:`, metrics);
+  const LCPdata = metrics?.metrics?.find((item: any) => item.name == "LCP");
+  console.log("LCPdata: ", LCPdata);
+
+  if (LCPdata) console.log(`${shop} received LCP:`, LCPdata?.value);
   return {
     body: "Metrics received",
   };
