736aa202976ffbad1e8b0410afe2e84c9cce1a1d
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2026-04-02 16:49:32 +0800

    修改SingleTextTranslat传参，添加resourceId字段
    修改SingleTextTranslat传参，添加resourceId字段

--- app/routes/app.manage_translation_.pagefly/route.tsx
+++ app/routes/app.manage_translation_.pagefly/route.tsx
@@ -551,6 +551,7 @@ const Index = () => {
       key: record?.key,
       type: record?.type,
       server: server || "",
+      resourceId: record?.resourceId,
     });
     if (data?.success) {
       if (loadingItemsRef.current.includes(record?.key)) {
