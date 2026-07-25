61cb0ed4e43e4e6b9c53df76b698758fa740425a
whoeven99 <whoeven99@outlook.com>
2026-07-25 23:50:46 +0800

    Stop tracking packages/translation-core/dist

--- packages/translation-core/dist/translationFilter/index.d.ts
+++
@@ -1,8 +0,0 @@
-export type { ExistingTranslation, IncludeFieldOptions, IncludeFieldV2Context, TranslatableContentInput, } from "./types.js";
-export { shouldIncludeFieldV2 } from "./shouldIncludeFieldV2.js";
-export { shouldIncludeFieldV3 as shouldIncludeField } from "./v3Base.js";
-export { translationRuleJudgment, shouldTranslateThemeKey, whiteListTranslate, metaTranslate } from "./judgeTranslateUtils.js";
-export { passesThemeModuleRules } from "./themeRules.js";
-export { passesMetafieldModuleRules } from "./metafieldRules.js";
-export { passesCoverAndOutdatedRules, translationNeedsRefresh } from "./v3Base.js";
-//# sourceMappingURL=index.d.ts.map
--- packages/translation-core/dist/translationFilter/themeRules.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Theme module branch (TranslateV2Service.needTranslate).
- */
-export declare function passesThemeModuleRules(module: string, key: string, value: string): boolean;
-//# sourceMappingURL=themeRules.d.ts.map
